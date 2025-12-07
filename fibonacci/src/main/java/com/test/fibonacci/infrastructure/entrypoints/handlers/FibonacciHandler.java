c6984ff948b3fd41093c152d3de7299b012d242d
Leandro Hernandez <l.hernandez071999@gmail.com>
2025-12-07 11:28:53 -0300

    Add: se crea handler para orquestar casos de uso

--- fibonacci/src/main/java/com/test/fibonacci/infrastructure/entrypoints/handlers/FibonacciHandler.java
+++ fibonacci/src/main/java/com/test/fibonacci/infrastructure/entrypoints/handlers/FibonacciHandler.java
@@ -1,9 +1,26 @@
 package com.test.fibonacci.infrastructure.entrypoints.handlers;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
+
+import com.test.fibonacci.domain.ports.in.GetNumberByExactMatchUseCase;
+import com.test.fibonacci.domain.ports.in.SaveNthNumberUseCase;
+import com.test.fibonacci.infrastructure.entrypoints.dtos.FibonacciDto;
 
 
 @Component
 public class FibonacciHandler {
+
+    private final SaveNthNumberUseCase saveNthNumberUseCase;
+    private final GetNumberByExactMatchUseCase getNumberByExactMatchUseCase;
+
+    public FibonacciHandler(SaveNthNumberUseCase saveNthNumberUseCase,GetNumberByExactMatchUseCase getNumberByExactMatchUseCase){
+        this.saveNthNumberUseCase=saveNthNumberUseCase;
+        this.getNumberByExactMatchUseCase=getNumberByExactMatchUseCase;
+    }
+    
+    public ResponseEntity<?> getNthNumber(FibonacciDto dto){
+        return null;
+    }
 
 }
