cd11da2a4330331ee9fa5dc322fea476dd02bf9c
Leandro Hernandez <l.hernandez071999@gmail.com>
2025-12-13 15:40:14 -0300

    Add: se corrige palabra

--- fibonacci/src/main/java/com/test/fibonacci/infrastructure/utils/GlobalConstants.java
+++ fibonacci/src/main/java/com/test/fibonacci/infrastructure/utils/GlobalConstants.java
@@ -2,5 +2,5 @@ package com.test.fibonacci.infrastructure.utils;
 
 public class GlobalConstants {
     public static final String ERROR_MESSAGE="An error occurred";
-    public static final String COMPLETE_OPERATION_MESSAGE="Operation finished succesfully";
+    public static final String COMPLETE_OPERATION_MESSAGE="Operation finished successfully";
 }
